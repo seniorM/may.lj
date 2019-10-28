e5416bd3767fc2fcf4dc7052c2e2d90b61a88e1a
seniorM <Global@mail.ua>
2019-10-28 21:06:40 +0200

    add index action

---
+++ app/controllers/MainController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace controllers;
+
+use core\AbstractController;
+
+class MainController extends AbstractController {
+
+    public function index() {
+	$this->view->render('');
+    }
+
+}
