dba0670220aec1503569ed66f5a4c2285d0343af
seniorM <Global@mail.ua>
2019-10-30 21:17:37 +0200

    add registration

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -3,6 +3,9 @@
 namespace controllers;
 
 use core\AbstractController;
+use mysqli;
+use core\Route;
+use models\AuthModel;
 
 class AuthController extends AbstractController {
 
@@ -10,4 +13,24 @@ class AuthController extends AbstractController {
 	$this->view->render('auth_index_view');
     }
 
+    public function registration() {
+	$this->view->render('auth_register_view');
+    }
+
+    public function regproc() {
+	if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+	    die('not available request method');
+	}
+	$user = filter_input_array(INPUT_POST);
+	//TODO все проверки
+	$model = $this->getModel();
+	$model->addUser($user);
+	Route::redirect(url('/auth'));
+    }
+    protected function getModel(){
+	if(!$this->model){
+	    $this->model = new AuthModel();
+	}
+	return $this->model;
+    }
 }
