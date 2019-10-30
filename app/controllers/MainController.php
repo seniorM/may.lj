b91df1b7051bea159fc1dee26a5bb7b8be381876
seniorM <Global@mail.ua>
2019-10-30 19:13:17 +0200

    call main_index_view.php as page template

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -7,7 +7,7 @@ use core\AbstractController;
 class MainController extends AbstractController {
 
     public function index() {
-	$this->view->render('');
+	$this->view->render('main_index_view');
     }
 
 }
