b7d9cd386dd8ff2b732c4fbf8d0b386ecfa85c0b
seniorM <Global@mail.ua>
2019-11-04 21:12:44 +0200

    big broun stack

---
+++ app/controllers/ApiController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace controllers;
+
+use core\AbstractController;
+
+class ApiController extends AbstractController {
+
+    public function index() {
+	
+    }
+
+    public function getauthors() {
+	echo'alert will be here soon';
+    }
+
+}
--- app/core/Route.php
+++ app/core/Route.php
@@ -10,67 +10,67 @@ class Route {
      * routing /controller/action
      */
     static public function init() {
-        //TODO create base routing
-        //######################################################################
-        //получить дополнительный путь и разбить его на элементы по / если есть 
-        //лишние элементы, то выдать 404. Если нет лишних, то первая часть - имя
-        //контроллера(класса), вторая часть - имя экшна(метода). Если элементов 
-        //меньше 2х, то экшн - index, контроллер main.Если указанного класса 
-        //контроллера не существует, или метода экшн, то 404. сделать метод 
-        //статический для ошибки 404
-        //######################################################################
-        //это значение останется в переменной, если не произойдёт перезапись
-        $controllerName = 'main';
-        //это значение останется в переменной, если не произойдёт перезапись
-        $actionName = 'index';
-        //забираем дополнительный путь документа в домене и разделяем его на список
+	//TODO create base routing
+	//######################################################################
+	//получить дополнительный путь и разбить его на элементы по / если есть 
+	//лишние элементы, то выдать 404. Если нет лишних, то первая часть - имя
+	//контроллера(класса), вторая часть - имя экшна(метода). Если элементов 
+	//меньше 2х, то экшн - index, контроллер main.Если указанного класса 
+	//контроллера не существует, или метода экшн, то 404. сделать метод 
+	//статический для ошибки 404
+	//######################################################################
+	//это значение останется в переменной, если не произойдёт перезапись
+	$controllerName = 'main';
+	//это значение останется в переменной, если не произойдёт перезапись
+	$actionName = 'index';
+	//забираем дополнительный путь документа в домене и разделяем его на список
 
-        $routeItems = explode('/', explode('?', $_SERVER['REQUEST_URI'])[0]);
-        //0-й элемент всегда содержит пустую строку, поэтому удаляем его 
-        array_shift($routeItems);
-        //проверяем последний элемент массива на пустое значение
-        //при необходимости удаляем его
-        if (empty($routeItems[count($routeItems) - 1])) {
-            array_pop($routeItems);
-        }
-        //если в массиве больше 2-х элементов, то 404
-        if (count($routeItems) > 2) {
-            self::error404();
-        }
-        if (!empty($routeItems[0])) {
-            //для кириллицы
-            //$controllerName = mb_strtolower(urldecode($routeItems[0]));
-            $controllerName = strtolower($routeItems[0]);
-        }
-        if (!empty($routeItems[1])) {
-            //для кириллицы
-            //$actionName = mb_strtolower(urldecode($routeItems[1]));д
-            $actionName = strtolower($routeItems[1]);
-        }
-        // для кириллицы
-        //$controllerClassName= mb_convert_case($controllerName, MB_CASE_TITLE);
-        $controllerClassName = 'controllers\\' . ucfirst($controllerName) . 'Controller';
-        if (!class_exists($controllerClassName)) {
-            self::error404();
-        }
-        $controller = new $controllerClassName();
-        if (!method_exists($controller, $actionName)) {
-            self::error404();
-        }
-        if ($controllerName !== 'auth' && !AuthModel::haveAuthUser()) {
-            Route::redirect(url('/auth'));
-        }
-        $controller->$actionName();
+	$routeItems = explode('/', explode('?', $_SERVER['REQUEST_URI'])[0]);
+	//0-й элемент всегда содержит пустую строку, поэтому удаляем его 
+	array_shift($routeItems);
+	//проверяем последний элемент массива на пустое значение
+	//при необходимости удаляем его
+	if (empty($routeItems[count($routeItems) - 1])) {
+	    array_pop($routeItems);
+	}
+	//если в массиве больше 2-х элементов, то 404
+	if (count($routeItems) > 2) {
+	    self::error404();
+	}
+	if (!empty($routeItems[0])) {
+	    //для кириллицы
+	    //$controllerName = mb_strtolower(urldecode($routeItems[0]));
+	    $controllerName = strtolower($routeItems[0]);
+	}
+	if (!empty($routeItems[1])) {
+	    //для кириллицы
+	    //$actionName = mb_strtolower(urldecode($routeItems[1]));д
+	    $actionName = strtolower($routeItems[1]);
+	}
+	// для кириллицы
+	//$controllerClassName= mb_convert_case($controllerName, MB_CASE_TITLE);
+	$controllerClassName = 'controllers\\' . ucfirst($controllerName) . 'Controller';
+	if (!class_exists($controllerClassName)) {
+	    self::error404();
+	}
+	$controller = new $controllerClassName();
+	if (!method_exists($controller, $actionName)) {
+	    self::error404();
+	}
+	if ($controllerName !== 'auth' && !AuthModel::haveAuthUser()) {
+	    Route::redirect(url('/auth'));
+	}
+	$controller->$actionName();
     }
 
     /**
      * create http status 404
      */
     static public function error404() {
-        http_response_code(404);
-        $view = new View();
-        $view->render('error_404_view');
-        exit();
+	http_response_code(404);
+	$view = new View();
+	$view->render('error_404_view');
+	exit();
     }
 
     /**
@@ -79,7 +79,7 @@ class Route {
      * @param string $url
      */
     static public function redirect(string $url) {
-        header('Location:' . $url);
+	header('Location:' . $url);
     }
 
     /**
@@ -87,13 +87,13 @@ class Route {
      * @return errors
      */
     static public function getErrors() {
-        //$errors=empty($_SESSION['errors']) ? [] : $_SESSION['errors'];
-        if (empty($_SESSION['errors'])) {
-            $errors = [];
-        } else {
-            $errors = $_SESSION['errors'];
-        }
-        return $errors;
+	//$errors=empty($_SESSION['errors']) ? [] : $_SESSION['errors'];
+	if (empty($_SESSION['errors'])) {
+	    $errors = [];
+	} else {
+	    $errors = $_SESSION['errors'];
+	}
+	return $errors;
     }
 
     /**
@@ -102,32 +102,32 @@ class Route {
      * @return string
      */
     static public function userValidate($user) {
-        $errors = [];
-        if (strlen($user['login']) < 6) {
-            $errors[] = 'Too short login value';
-        }
-        if ($user['pass'] !== $user['pass_conf']) {
-            $errors[] = 'Password do not match';
-        } else if (strlen($user['pass']) < 6) {
-            $errors[] = 'Too short password value';
-        }
-        if (empty($user['email'])) {
-            $errors[] = 'Email is required field';
-        }
-        if (!filter_var($user['email'], FILTER_VALIDATE_EMAIL)) {
-            $errors[] = 'Incorrect email value';
-        }
-        return $errors;
+	$errors = [];
+	if (strlen($user['login']) < 6) {
+	    $errors[] = 'Too short login value';
+	}
+	if ($user['pass'] !== $user['pass_conf']) {
+	    $errors[] = 'Password do not match';
+	} else if (strlen($user['pass']) < 6) {
+	    $errors[] = 'Too short password value';
+	}
+	if (empty($user['email'])) {
+	    $errors[] = 'Email is required field';
+	}
+	if (!filter_var($user['email'], FILTER_VALIDATE_EMAIL)) {
+	    $errors[] = 'Incorrect email value';
+	}
+	return $errors;
     }
 
     /**
      * clear session
      */
     static public function clearSession() {
-        //чтобы не сохранились к следующему запуску
-        $_SESSION['errors'] = null;
-        $_SESSION['login'] = null;
-        $_SESSION['email'] = null;
+	//чтобы не сохранились к следующему запуску
+	$_SESSION['errors'] = null;
+	$_SESSION['login'] = null;
+	$_SESSION['email'] = null;
     }
 
 }
