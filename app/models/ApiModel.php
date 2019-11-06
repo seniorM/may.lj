665c4eda6790f8e38d35c7ac2998ac28e704d011
seniorM <Global@mail.ua>
2019-11-06 18:21:07 +0200

    add ApiModel.php

---
+++ app/models/ApiModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace models;
+
+use core\AbstractModel;
+
+class ApiModel extends AbstractModel {
+
+    public function getAuthors() {
+	$query = " select * from users;";
+	$result = $this->db->query($query);
+	if (!$result) {
+	    die($this->db->error);
+	}
+	$authors = $result->fetch_all(MYSQLI_ASSOC);
+	return $authors;
+    }
+
+}
